61bf6b3bd40fb3c04baa7487e07cba1be033e347
Chiha <code.achsh@gmail.com>
2025-02-01 20:11:28 +0900

    Initial commit

---
+++ assets/js/jquery.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+    // Jquery Methods go
+
+    // INTRODUCTION
+    $(document).ready(function() {
+        $('.image-placeholder').html('<img src="assets/images/resume-photo.jpg" alt="Resume Photo">');
+    });
+    
+   
